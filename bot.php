64768403d55a64d751865e9272d92bcfd6204302
nirasnet <nirasnet@gmail.com>
2017-08-31 22:48:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,47 +1,8 @@
 <?php
-$access_token = 'GisRbNPenbT2yUm8+AbDWIYgXZX17GoYa4FbBlia9cB7xMj87uzTTY7vVQO0/tHd/qZhdtT+AnOdurOvKaR4LM/PQpxkQsL+t7+msL1fE63c2RNyIIkufnpLjj0xxIObLXv1JhdXcq9Fw5Cy3ZOuFgdB04t89/1O/w1cDnyilFU=';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('GisRbNPenbT2yUm8+AbDWIYgXZX17GoYa4FbBlia9cB7xMj87uzTTY7vVQO0/tHd/qZhdtT+AnOdurOvKaR4LM/PQpxkQsL+t7+msL1fE63c2RNyIIkufnpLjj0xxIObLXv1JhdXcq9Fw5Cy3ZOuFgdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '1ce56e8d64d66d5ad38f79f4c973e1e8']);
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
- // Loop through each event
- foreach ($events['events'] as $event) {
-  // Reply only when message sent is in 'text' format
-  if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-   // Get text sent
-   $text = $event['message']['text'];
-   // Get replyToken
-   $replyToken = $event['replyToken'];
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
 
-   // Build message to reply back
-   $messages = [
-    'type' => 'text',
-    'text' => $text
-   ];
-
-   // Make a POST Request to Messaging API to reply to sender
-   $url = 'https://api.line.me/v2/bot/message/reply';
-   $data = [
-    'replyToken' => $replyToken,
-    'messages' => [$messages],
-   ];
-   $post = json_encode($data);
-   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-   $ch = curl_init($url);
-   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-   $result = curl_exec($ch);
-   curl_close($ch);
-
-   echo $result . "\r\n";
-  }
- }
-}
-echo "OK";
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
