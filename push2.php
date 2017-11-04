7d01f34c4ad484488fa3081ad48753e36e45c30a
nirasnet <nirasnet@gmail.com>
2017-11-04 16:15:54 +0700

    Update and rename push.php to push2.php

--- push2.php
+++ push2.php
@@ -1,6 +1,7 @@
 <?php
  
-$strAccessToken = "GisRbNPenbT2yUm8+AbDWIYgXZX17GoYa4FbBlia9cB7xMj87uzTTY7vVQO0/tHd/qZhdtT+AnOdurOvKaR4LM/PQpxkQsL+t7+msL1fE63c2RNyIIkufnpLjj0xxIObLXv1JhdXcq9Fw5Cy3ZOuFgdB04t89/1O/w1cDnyilFU=";
+//$strAccessToken = "GisRbNPenbT2yUm8+AbDWIYgXZX17GoYa4FbBlia9cB7xMj87uzTTY7vVQO0/tHd/qZhdtT+AnOdurOvKaR4LM/PQpxkQsL+t7+msL1fE63c2RNyIIkufnpLjj0xxIObLXv1JhdXcq9Fw5Cy3ZOuFgdB04t89/1O/w1cDnyilFU=";
+$strAccessToken = "avME3fzE4pEizgQLBXWSVDOrgIpJBHDroI/RRC+vafeGvqBg9nlW5d2ajywSn5jY3TpUvOLw0ZUgqzLXNrIv/ffjrBEwWAeDjQB0P/3TQHi1Vo4+fiWIwyB585g4FinojhkAALbomQ58ZCGsEYYVKQdB04t89/1O/w1cDnyilFU=";
  
 $strUrl = "https://api.line.me/v2/bot/message/push";
  
