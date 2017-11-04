33d070390131c0d1a3270efbb564b39dd9a2f6db
nirasnet <nirasnet@gmail.com>
2017-11-04 16:29:05 +0700

    Update push2.php

--- push2.php
+++ push2.php
@@ -2,7 +2,8 @@
  
 //$strAccessToken = "GisRbNPenbT2yUm8+AbDWIYgXZX17GoYa4FbBlia9cB7xMj87uzTTY7vVQO0/tHd/qZhdtT+AnOdurOvKaR4LM/PQpxkQsL+t7+msL1fE63c2RNyIIkufnpLjj0xxIObLXv1JhdXcq9Fw5Cy3ZOuFgdB04t89/1O/w1cDnyilFU=";
 $strAccessToken = "avME3fzE4pEizgQLBXWSVDOrgIpJBHDroI/RRC+vafeGvqBg9nlW5d2ajywSn5jY3TpUvOLw0ZUgqzLXNrIv/ffjrBEwWAeDjQB0P/3TQHi1Vo4+fiWIwyB585g4FinojhkAALbomQ58ZCGsEYYVKQdB04t89/1O/w1cDnyilFU=";
- 
+//$strAccessToken = "avME3fzE4pEizgQLBXWSVDOrgIpJBHDroI/RRC+vafeGvqBg9nlW5d2ajywSn5jY3TpUvOLw0ZUgqzLXNrIv/ffjrBEwWAeDjQB0P/3TQHi1Vo4+fiWIwyB585g4FinojhkAALbomQ58ZCGsEYYVKQdB04t89/1O/w1cDnyilFU=";
+
 $strUrl = "https://api.line.me/v2/bot/message/push";
  
 $arrHeader = array();
@@ -10,7 +11,7 @@ $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
 $arrPostData = array();
-$arrPostData['to'] = "Uef838b89036889d3410cc7f61a942cae";
+$arrPostData['to'] = "U56412033af84eef2f82db90e816a1ad3";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
  
